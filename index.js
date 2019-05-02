3ff2c60764bea0bb0adfc08e74b67e040ebbd007
NathanielRichardson <49959240+NathanielRichardson@users.noreply.github.com>
2019-05-01 21:32:20 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/**
- * @format
- */
+import React, { Component } from 'react';
+import { Root, Tabs } from './config/router';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+class App extends Component {
+  render() {
+    return <Root />;
+  }
+}
 
-AppRegistry.registerComponent(appName, () => App);
+export default App;
